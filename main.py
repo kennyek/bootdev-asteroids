691bcdf22fd066ed14ccf89a92c07a51bd890a37
Kenny Ek <kennyswek@gmail.com>
2024-10-07 12:27:16 +0200

    feat: Limit game loop to 60 FPS

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import pygame
 from constants import *
 
 def main():
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-  screen.fill(pygame.Color("black"))
-  pygame.display.flip()
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
+      screen.fill(pygame.Color("black"))
+      pygame.display.flip()
+
+      dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
